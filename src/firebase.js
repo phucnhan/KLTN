86fafd6d185439e948bab8f71833ab2e34a0bfa2
Nguyễn Xuân Thịnh <nguyenxuanthinh345@gmail.com>
2024-10-14 11:16:44 +0700

    1

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, si
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDOe1lAYHG_xOCZHLdBDWAdKEe_4sg0WYM",
-    authDomain: "fir-c8ee2.firebaseapp.com",
-    projectId: "fir-c8ee2",
-    storageBucket: "fir-c8ee2.appspot.com",
-    messagingSenderId: "949915778545",
-    appId: "1:949915778545:web:21486f967991101300d850",
-    measurementId: "G-N8LSXX52ZD"
-  };
+  apiKey: "AIzaSyDOe1lAYHG_xOCZHLdBDWAdKEe_4sg0WYM",
+  authDomain: "fir-c8ee2.firebaseapp.com",
+  projectId: "fir-c8ee2",
+  storageBucket: "fir-c8ee2.appspot.com",
+  messagingSenderId: "949915778545",
+  appId: "1:949915778545:web:21486f967991101300d850",
+  measurementId: "G-N8LSXX52ZD"
+};
 
 
 const app = initializeApp(firebaseConfig);
