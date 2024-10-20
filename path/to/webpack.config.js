d07e9b7ff7a709ffce9664b06ce61b5276b71fda
phucnhan <phucnhan0406@gmail.com>
2024-10-20 16:53:30 +0700

    1

---
+++ path/to/webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    devServer: {
+        allowedHosts: [
+            'localhost', // Add your allowed host here
+            // You can add more hosts if needed
+        ],
+    },
+};
